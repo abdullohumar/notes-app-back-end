926374592abc37b0dbec2184f78a481ea48e4014
Abdulloh Umar <iniautok@gmail.com>
2025-09-15 13:52:57 +0700

    Menyimpan Catatan

---
+++ src/handler.js
@@ -0,0 +1,43 @@
+const { nanoid } = require("nanoid");
+const notes = require("./notes");
+
+const addNotesHandler = (req, h) => {
+  const { title, tag, body } = req.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title,
+    tag,
+    body,
+    id,
+    createdAt,
+    updatedAt,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: "success",
+      message: "Berhasil menambah catatan",
+      data: {
+        noteId: id,
+      },
+    });
+    response.code("201");
+    return response;
+  }
+  const response = h.response({
+    status: "fail",
+    message: "Catatan gagal ditambahkan",
+  });
+  response.code(500);
+  return response;
+};
+
+module.exports = { addNotesHandler };
